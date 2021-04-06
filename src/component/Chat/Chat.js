7893b7ee7ed763a1276fb15ce8fbaf10572b917c
simonchen92 <simon.k.chen92@gmail.com>
2021-04-06 15:02:25 -0400

    Add custom domain for deployment

--- src/component/Chat/Chat.js
+++ src/component/Chat/Chat.js
@@ -33,7 +33,7 @@ const Chat = ({ location }) => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://chatter-box-1.herokuapp.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
